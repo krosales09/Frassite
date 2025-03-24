a8449f566fabbaa26ecc4e416b9e890f2f7d8cf4
krosales09 <krosalestauca@gmail.com>
2025-03-24 17:10:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
 let carrito = [];
-let total = 0;
 
-function agregarAlCarrito(producto, precio) {
-    carrito.push({ producto, precio });
-    total += precio;
+function agregarAlCarrito(producto) {
+    carrito.push(producto);
     actualizarCarrito();
 }
 
 function actualizarCarrito() {
-    let listaCarrito = document.getElementById("lista-carrito");
-    let totalElemento = document.getElementById("total");
-    
+    const listaCarrito = document.getElementById("lista-carrito");
     listaCarrito.innerHTML = "";
-    carrito.forEach((item, index) => {
-        let li = document.createElement("li");
-        li.textContent = `${item.producto} - $${item.precio}`;
-        let botonEliminar = document.createElement("button");
-        botonEliminar.textContent = "X";
-        botonEliminar.onclick = () => eliminarDelCarrito(index);
-        li.appendChild(botonEliminar);
+    carrito.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
         listaCarrito.appendChild(li);
     });
-    
-    totalElemento.textContent = total;
-}
-
-function eliminarDelCarrito(index) {
-    total -= carrito[index].precio;
-    carrito.splice(index, 1);
-    actualizarCarrito();
 }
